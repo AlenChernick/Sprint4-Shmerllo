570e7355b84a7468c7a42d38e7f02058425de1d5
unknown <crazyguy771@gmail.com>
2022-07-20 10:39:30 +0300

    added groupList and groupPreviw

--- src/components/board-preview.vue
+++ src/components/board-preview.vue
@@ -1,17 +1,23 @@
 <template>
   <section class="board-preview">
     <h2>This is board preview</h2>
-    <pre>{{getBoards}}</pre>
+    <pre>{{ getBoards }}</pre>
+    <group-list />
   </section>
 </template>
 <script>
+import groupList from '../components/group-list.vue'
+
 export default {
   name: 'board-preview',
-  computed:{
-    getBoards(){
-    return  this.$store.getters.getBoards
-    }
-  }
+  computed: {
+    getBoards() {
+      return this.$store.getters.getBoards
+    },
+  },
+  components: {
+    groupList,
+  },
 }
 </script>
 <style lang=""></style>
