9e5217960311c5d00082452f7d3e700d9ecc6965
Alon Kolker <alonkolk@gmail.com>
2022-07-20 14:30:33 +0300

    dnd integration in group list and group prev

--- src/components/group-list.vue
+++ src/components/group-list.vue
@@ -1,21 +1,48 @@
 <template lang="">
   <section class="group-list">
     <h2>this is group list</h2>
-    <group-preview v-for="group in groups" :group="group" :key="group.id" />
+    <Container class="group-conteiner"  @drop="onDrop">
+      <Draggable>
+        <group-preview v-for="group in groups" :group="group" :key="group.id" />
+      </Draggable>
+      </Container>
   </section>
 </template>
 <script>
 import groupPreview from '../components/group-preview.vue'
+import { Container, Draggable } from "vue3-smooth-dnd"
 
 export default {
-  name: 'group-list',
+  name: "group-list",
   props: {
     groups: {
       type: Array,
     },
   },
+    methods: {  
+    onDrop(dropResult){
+      this.items = this.applyDrag(this.items, dropResult);
+    },
+    applyDrag(arr, dragResult){
+      const { removedIndex, addedIndex, payload } = dragResult;
+
+      if (removedIndex === null && addedIndex === null) return arr;
+      const result = [...arr];
+      let itemToAdd = payload;
+      
+      if (removedIndex !== null) {
+        itemToAdd = result.splice(removedIndex, 1)[0];
+      }
+      if (addedIndex !== null) {
+        result.splice(addedIndex, 0, itemToAdd);
+      }
+      return result;
+    }
+  },
   components: {
     groupPreview,
+    Container,
+    Draggable,
   },
 }
 </script>
