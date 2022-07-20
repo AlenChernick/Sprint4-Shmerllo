3b35a2eadcb3ba6a7275b272cd760c541720c171
unknown <crazyguy771@gmail.com>
2022-07-20 10:21:51 +0300

    added board-list

---
+++ src/components/board-list.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="board-list"></section>
+  <board-preview />
+</template>
+<script>
+import boardPreview from '../components/board-preview.vue'
+
+export default {
+  name: 'board-list',
+  data() {
+    return {}
+  },
+  components: {
+    boardPreview,
+  },
+}
+</script>
+<style lang=""></style>
