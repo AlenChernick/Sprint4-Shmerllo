98deb84d40032ead217d6f2c82130a6027e47f4f
unknown <crazyguy771@gmail.com>
2022-07-20 14:19:53 +0300

    added layout added app-header-nav

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,9 +1,10 @@
-<template lang="">
-  <header class="app-header">
-    <h1>Let's rock!</h1>
-    <router-link to="/">Home</router-link>
-    <router-link to="/board">Boards</router-link>
-    <router-link to="/login">Login</router-link>
+<template>
+  <header class="app-header main-header full">
+    <router-link class="home-page-btn" to="/"><img src="../assets/img/logo.png" alt="" /></router-link>
+    <router-link class="boards-page-btn" to="/board"
+      ><font-awesome-icon class="board-icon" icon="fab fa-trello" />Boards</router-link
+    >
+    <router-link class="login-page-btn" to="/login">Login</router-link>
   </header>
 </template>
 <script>
