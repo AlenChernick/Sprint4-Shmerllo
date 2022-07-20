5391e16879f0337776edca07a90f79537674afe2
unknown <crazyguy771@gmail.com>
2022-07-20 16:16:47 +0300

    group-container scss changed

--- src/components/task-list.vue
+++ src/components/task-list.vue
@@ -1,6 +1,7 @@
 <template>
-  <section class="task-list"></section>
-  <task-preview v-for="task in tasks" :task="task" :key="task.id" />
+  <section class="task-list">
+    <task-preview v-for="task in tasks" :task="task" :key="task.id" />
+  </section>
 </template>
 <script>
 import taskPreview from '../components/task-preview.vue'
