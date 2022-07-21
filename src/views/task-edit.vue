cbd46c813375de67f52a7a5873eb714a2a275827
unknown <crazyguy771@gmail.com>
2022-07-21 15:51:45 +0300

    task-edit ui/ux almost finshed

--- src/views/task-edit.vue
+++ src/views/task-edit.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="task-edit-header">
       <span><font-awesome-icon icon="fa-solid fa-pager" /></span>
-      <input v-model="task.title" type="text" />
+      <input spellcheck="false" v-model="task.title" type="text" />
     </div>
     <div class="task-list-name">
       <p>in List... to add when nestedroute</p>
@@ -28,53 +28,85 @@
             </li>
           </ul>
           <div class="main-editor-dates">
-            <h4>Dates:</h4>
-            <p>CreatedAt: {{ new Date(task.createdAt).toString() }}</p>
-            <p>DueDate: {{ new Date(task.dueDate).toString() }}</p>
+            Dates
+            <div class="main-editor-dates-picker">
+              <el-checkbox class="main-editor-checkbox"></el-checkbox>
+              <el-date-picker type="dates" v-model="dateValue"></el-date-picker>
+            </div>
           </div>
         </div>
         <div class="main-editor-description">
           <span><font-awesome-icon icon="fa-solid fa-bars" /></span>
           <h4 class="main-editor-description-title">Description</h4>
+          <el-button class="btn main-editor-decription-edit-btn" type="info">Edit</el-button>
         </div>
-        <textarea @click="onEdit" v-model="task.description" class="main-editor-description-info">{{
-          task.description
-        }}</textarea>
+        <textarea spellcheck="false" @click="onEdit" v-model="task.description" class="main-editor-description-info"
+          >{{ task.description }}
+        </textarea>
         <div class="main-editor-btn-container">
-          <el-button class="btn" type="primary" v-if="isEdit === true" @click="saveTask">Save</el-button>
-          <el-button type="info" v-if="isEdit === true" @click="isEdit = false">Cancel</el-button>
+          <el-button type="primary" v-if="isEdit === true" @click="saveTask">Save</el-button>
+          <el-button type="info" class="btn" v-if="isEdit === true" @click="isEdit = false">Cancel</el-button>
         </div>
-        <h4>Status: {{ task.status }}</h4>
-        <ul v-for="comment in task.comments">
+        <div class="main-editor-checklist-container" v-for="checklist in task.checklists">
+          <div class="main-editor-checklist-header">
+            <div class="main-editor-checklist-header-info">
+              <span class="main-editor-checklist-icon"><font-awesome-icon icon="fa-solid fa-list-check" /></span>
+              <h4 class="main-editor-checklist-title">{{ checklist.title }}</h4>
+            </div>
+            <el-button type="info" class="btn main-editor-checklist-delete-btn">Delete</el-button>
+          </div>
+          <ul v-for="todo in checklist.todos">
+            <li>
+              <el-checkbox>{{ todo.title }}</el-checkbox>
+              <!-- <p>{{ todo.isDone }}</p> -->
+            </li>
+          </ul>
+          <el-button type="info" class="btn main-editor-add-item-btn">Add an item</el-button>
+        </div>
+        <div class="main-editor-activity-contianer">
+          <div class="main-editor-activity-header">
+            <div class="main-editor-activity-header-info">
+              <span class="main-editor-activity-icon"><font-awesome-icon icon="fa-solid fa-list-ul" /></span>
+              <h4 class="main-editor-activity-title">Activity</h4>
+            </div>
+            <el-button class="btn" type="info">Show details</el-button>
+          </div>
+          <div class="main-editor-activity-comments">
+            <span class="main-editor-activity-icon"><font-awesome-icon icon="fa-solid fa-user" /></span>
+            <textarea
+              @click="onWriteComment"
+              placeholder="Write a comment"
+              spellcheck="false"
+              class="main-editor-activity-comment"
+            >
+            </textarea>
+          </div>
+        </div>
+        <!-- <ul v-for="comment in task.comments">
           Activity
           <li>
             <p>{{ comment.byMember.fullName }}</p>
             <p>{{ comment.txt }}</p>
             <p>{{ new Date(comment.createdAt).toString() }}</p>
-            <!-- <img :src="comment.byMember.imgUrl"/> -->
+            <img :src="comment.byMember.imgUrl" />
           </li>
-        </ul>
-        <div v-for="checklist in task.checklists">
-          checkLists:
-          <h4>{{ checklist.title }}</h4>
-          <ul v-for="todo in checklist.todos">
-            Activity
-            <li>
-              <p>{{ todo.title }}</p>
-              <p>{{ todo.isDone }}</p>
-            </li>
-          </ul>
-        </div>
+        </ul> -->
       </div>
       <div class="main-task-sidebar">
-        <button @click="saveTask">SaveTask</button>
-        <button @click="removeTask">RemoveTask</button>
+        <h6>Add to card</h6>
+        <div class="main-task-edit-btn"><h5>Members</h5></div>
+        <div class="main-task-edit-btn"><h5>Labels</h5></div>
+        <div class="main-task-edit-btn"><h5>Checklist</h5></div>
+        <div class="main-task-edit-btn"><h5>Dates</h5></div>
+        <div class="main-task-edit-btn"><h5>Attachment</h5></div>
       </div>
     </div>
     <!-- <pre>{{task}}</pre> -->
   </section>
 </template>
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'task-edit',
   data() {
@@ -83,6 +115,8 @@ export default {
       boardId: null,
       groupId: null,
       isEdit: false,
+      isWrite: false,
+      dateValue: ref(''),
     }
   },
   async created() {
@@ -106,6 +140,9 @@ export default {
       console.log(this.task.id)
       this.$store.dispatch({ type: 'removeTask', taskId: this.task.id, groupId: this.groupId, boardId: this.boardId })
       this.$router.push('/board' + boardId)
+    },
+    onWriteComment() {
+      this.isWrite = !this.isWrite
     },
     onEdit() {
       this.isEdit = true
