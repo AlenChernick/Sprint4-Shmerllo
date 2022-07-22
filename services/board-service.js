8fb877cc9f59ee07ebff3d162017309d6cf4e1de
Tal Hammer Topaz <talhtopaz@gmail.com>
2022-07-22 17:20:15 +0300

    cover choice

--- services/board-service.js
+++ services/board-service.js
@@ -3,6 +3,7 @@ import { storageService } from './storage-service.js'
 import { utilService } from './util-service.js'
 
 
+
 const STORAGE_KEY = 'board_db'
 const TASK = 'task'
 
@@ -21,6 +22,7 @@ export const boardService = {
     saveTask,
     removeTask,
     removeGroup,
+    coverOptions,
 }
 
 //get boards
@@ -238,13 +240,10 @@ function getEmptyBoard() {
     return {
         _id: '',
         title: '',
-        // style: {
-        //     type: '',
+         style: {
              backgroundImage: 'https://webneel.com/daily/sites/default/files/images/daily/08-2018/1-nature-photography-spring-season-mumtazshamsee.jpg',
-        //     backgroundThumb: url('https://images.unsplash.com/photo-1542831371-29b0f74f9713?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyODE5MzB8MHwxfHNlYXJjaHwzfHxjb2Rpbmd8ZW58MHx8fHwxNjQyMzU4NjIz&ixlib=rb-1.2.1&q=80&w=200'),
-        //     isDark: true,
-        //     customImages: []
-        // },
+             backgroundColor: 'red',
+        },
         members: [
             {
                 id: 'm101',
@@ -505,6 +504,23 @@ function getEmptyBoard() {
 
 
 function coverOptions(){
-    return{}
+    return{
+        coverColors: [ '#277da1', '#4d908e', '#fb6f92', '#90be6d', '#f9c74f',
+                       '#f9844a' ,'#00b4d8' , '#3a5a40'],
+        coverImgs: ['https://webneel.com/daily/sites/default/files/images/daily/08-2018/1-nature-photography-spring-season-mumtazshamsee.jpg',
+                       'https://media.istockphoto.com/photos/the-sun-goes-down-behind-the-autumn-forest-picture-id1162998855?k=20&m=1162998855&s=612x612&w=0&h=JLbCH4hLaO5war1ipJXx7eoxXMdhcMXFO9pwXz1NR1Q=',
+                    'https://wallpaperaccess.com/full/1131217.jpg',
+                    'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/close-up-of-tulips-blooming-in-field-royalty-free-image-1584131603.jpg',
+                    'https://media.istockphoto.com/photos/hydrangea-flowers-in-the-garden-picture-id927499422?k=20&m=927499422&s=612x612&w=0&h=9fZtBAXZ3I8qNRTi87SHTDEjbBjyn_eRoDoLAC7zVvg=',
+                    'https://i.pinimg.com/originals/18/28/4e/18284ec99d85c9ee5afaf05baf77083a.jpg',
+                    'https://media.istockphoto.com/photos/colored-ceiling-picture-id1208301897?k=20&m=1208301897&s=612x612&w=0&h=xXFlsJphxez3hgCYRxYmS7yxb5P4-HOtbnsjIVJSSWA=',
+                    'https://assets.weforum.org/global_future_council/image/xALg-7b0WN5aLOY6aejbKW3NepG-PEipzKnEuyS8ZlI.jpeg',
+                    'https://media.cntraveler.com/photos/5ca60f7f7b531a5e47949cde/master/w_4000,h_2400,c_limit/NYC_GettyImages-500619014.jpg',
+                    'https://images.pexels.com/photos/460621/pexels-photo-460621.jpeg?auto=compress&cs=tinysrgb&w=1600',
+
+
+                 ],
+
+    }
 
 }
