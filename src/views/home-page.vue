32d89c6f580f821565834a4e401f0e44b467957b
Tal Hammer Topaz <talhtopaz@gmail.com>
2022-07-22 20:26:29 +0300

    home page pic

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,6 +1,7 @@
 <template lang="">
   <section class="home-page">
     <h2>This is home page</h2>
+    <img src="https://img.freepik.com/premium-vector/business-teamwork-with-puzzle-concept-vector-illustration-man-woman-character-team-work-partnership-make-idea-together-female-person_109722-3475.jpg"/>
   </section>
 </template>
 <script>
