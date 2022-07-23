c089ddca102d68f900de44161a5a06beb6197ed2
Tal Hammer Topaz <talhtopaz@gmail.com>
2022-07-23 14:38:04 +0300

    home page

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,7 +1,12 @@
 <template lang="">
   <section class="home-page">
-    <h2>This is home page</h2>
+    <div calss="home-page-header">
+      <h2>Trello helps teams move work forward.</h2>
+      <h3>Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with Trello.</h3>
+      <button>Start demo</button>
+    </div>
     <img src="https://img.freepik.com/premium-vector/business-teamwork-with-puzzle-concept-vector-illustration-man-woman-character-team-work-partnership-make-idea-together-female-person_109722-3475.jpg"/>
+
   </section>
 </template>
 <script>
