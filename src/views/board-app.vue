04698ef21e58928cec336f60bd7b26a29e5a04aa
Tal Hammer Topaz <talhtopaz@gmail.com>
2022-07-23 18:07:12 +0300

    home page & bords page

--- src/views/board-app.vue
+++ src/views/board-app.vue
@@ -1,7 +1,14 @@
 <template lang="">
   <section class="board-app">
-    <h3 class="workspace-logo"><font-awesome-icon class="workspace-icon" icon="fab fa-trello" />Workspace</h3>
-    <board-list :boards="getBoards" />
+    <div class="starred-boards">
+      <span class="full-star-icon"></span>
+      <h3> Starred</h3>
+      <h5 v-if="!starredBoards.length">No starred boards yet...</h5>
+    <board-list :boards="starredBoards" />
+    </div>
+    <h3 class="workspace-logo">
+      <font-awesome-icon class="workspace-icon" icon="fab fa-trello" />Your workspaces</h3>
+    <board-list :boards="nonStarredBoards" />
   </section>
 </template>
 <script>
@@ -16,6 +23,12 @@ export default {
     getBoards() {
       return this.$store.getters.getBoards
     },
+    starredBoards(){
+      return this.$store.getters.getBoards.filter((board) => board.isFavorite)
+    },
+    nonStarredBoards(){
+      return this.$store.getters.getBoards.filter((board) => !board.isFavorite)
+    }
   },
 }
 </script>
