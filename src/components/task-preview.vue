25f041afb51792e1378e4badada3908270ffd013
Tal Hammer Topaz <talhtopaz@gmail.com>
2022-07-24 09:26:12 +0300

    quick edit UI

--- src/components/task-preview.vue
+++ src/components/task-preview.vue
@@ -1,9 +1,37 @@
 <template>
   <section @click="openTaskDetails">
 
-    <div>{{ task.title }}</div>
+    <span @click.stop="quickEditDisplay = 'block'"  class="edit-icon">
+    <font-awesome-icon icon="fa-solid fa-pen" /></span>
+    <div><h5>{{ task.title }}</h5></div>
     <div v-if="task.style?.bgImgUrl"><img :src ="task.style.bgImgUrl" alt=""></div>
+
+
+    <div @click.stop="quickEditDisplay = 'none'"  
+    class="quickEditScreen" :style="{ display: quickEditDisplay}">
+       </div>
+     
+     <div class="quickEdit" :style="{ display: quickEditDisplay}">
+
+        <div class="title-edit">
+          <h5>{{task.title}}</h5>
+        </div>
+      
+      <div class="action-buttons">
+        
+        <button><span class="card-icon"></span>Open card</button>
+        <button><span class="labels-icon"></span>Edit lables</button>
+        <button><span class="members-icon"></span>Change members</button>
+        <button><span class="cover-icon"></span>Change cover</button>
+        <button><span class="dates-icon"></span>Edit dates</button>
+        <button><span class="archive-icon"></span>Archive</button>
+      </div>
+
+      <div class="save-button">
+        <button>Save</button>
+      </div>
  
+    </div>  
   </section>
 </template>
 <script>
@@ -16,6 +44,12 @@ export default {
     groupId: {
       type: String,
     },
+  },
+  data() {
+    return {
+       quickEditDisplay: 'none',
+
+    }
   },
   methods: {
     openTaskDetails() {
