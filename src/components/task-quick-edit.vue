5d7c6b987c1cc033677d7f95461b3181331882db
Tal Hammer Topaz <talhtopaz@gmail.com>
2022-07-24 14:10:27 +0300

    task-preview

--- src/components/task-quick-edit.vue
+++ src/components/task-quick-edit.vue
@@ -1,21 +1,16 @@
 <template>
-  <section @click="openTaskDetails">
+
 
     <span @click.stop="quickEditDisplay = 'block'"  class="edit-icon">
     <font-awesome-icon icon="fa-solid fa-pen" /></span>
-    <div><h5>{{ task.title }}</h5></div>
-    <div v-if="task.style?.bgImgUrl"><img :src ="task.style.bgImgUrl" alt=""></div>
+  
 
-
-
-
-<!-- Tal's part -->
 
     <div @click.stop="quickEditDisplay = 'none'"  
     class="quickEditScreen" :style="{ display: quickEditDisplay}">
        </div>
      
-     <div class="quickEdit" :style="{ display: quickEditDisplay}">
+     <section class="quickEdit" :style="{ display: quickEditDisplay}">
 
         <div class="title-edit">
          <img v-if="task.style?.bgImgUrl" :src="task.style.bgImgUrl" />
@@ -24,24 +19,26 @@
       
       <div class="action-buttons">
         
-        <button @click="openTaskDetails"><span class="card-icon"></span>Open card</button>
-        <button @click="editLabels"><span class="labels-icon"></span>Edit lables</button>
-        <button @click="editMembers"><span class="members-icon"></span>Change members</button>
-        <button  @click="changeCover"><span class="cover-icon"></span>Change cover</button>
-        <button @click="editDates"><span class="dates-icon"></span>Edit dates</button>
-        <button @click="removeTask"><span class="archive-icon"></span>Archive</button>
+        <button><span class="card-icon"></span>Open card</button>
+        <button><span class="labels-icon"></span>Edit lables</button>
+        <button><span class="members-icon"></span>Change members</button>
+        <button><span class="cover-icon"></span>Change cover</button>
+        <button><span class="dates-icon"></span>Edit dates</button>
+        <button><span class="archive-icon"></span>Archive</button>
       </div>
 
       <div class="save-button">
         <button>Save</button>
       </div>
  
-    </div>  
+  
   </section>
 </template>
 <script>
+
+
 export default {
-  name: "task-preview",
+  name: "task-quick-edit",
   props: {
     task: {
       type: Object,
@@ -52,7 +49,7 @@ export default {
   },
   data() {
     return {
-       quickEditDisplay: 'none',
+       quickEditDisplay: 'block',
 
     }
   },
@@ -66,15 +63,14 @@ export default {
       )
       // this.$router.push('')
     },
-    removeTask(){
-      console.log('remove task')
-      this.$store.dispatch({type:'removeTask', taskId: this.task.id})
-    }
   },
   computed: {
     getCurrBoard() {
       return this.$store.getters.getCurrBoard
     },
+  },
+  components: {
+    taskQuickEdit,
   },
 }
 </script>
