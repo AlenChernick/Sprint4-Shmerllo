f9a1fc268edd94cf601f6d9b5e48f2637069ce62
Alon Kolker <alonkolk@gmail.com>
2022-07-25 09:40:44 +0300

    progress bar works

--- src/views/task-edit.vue
+++ src/views/task-edit.vue
@@ -118,7 +118,7 @@
           </div>
            <div  class="checklist-progressbar-contianer">
             <el-progress
-              :percentage="90"
+              :percentage="doneTodos(checklist)"
               class="checklist-progressbar"
             />
           </div>
@@ -336,6 +336,13 @@ export default {
       const label = boardLabels.find((l) => l.id === labelId)
       return label.bgColor
     },
+       doneTodos(checklist) {
+      let chackListLen = checklist.todos.length
+      let complited = checklist.todos.filter(todo=> todo.isDone === true)
+      let commentLen = complited.length
+console.log(commentLen,chackListLen);
+      return (commentLen/chackListLen)*100
+    },
   },
   computed: {
     getCurrTask() {
@@ -350,11 +357,7 @@ export default {
     // boards() {
     //   return JSON.parse(JSON.stringify(this.$store.getters.getBoards))
     // },
-    doneTodos() {
-      // let chackListLen = checkList.todos.length
-      // let complited = checkList.todos.filter(todo=> todo.isDone === true)
-      return 50
-    },
+
   },
   components: {
     editTaskActions,
