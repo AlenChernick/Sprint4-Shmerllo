1d295cf0d72738a3dfcd4c943f9cf96e7848629e
Tal Hammer Topaz <talhtopaz@gmail.com>
2022-07-31 13:52:06 +0300

    fix quick edit

--- src/components/task-preview.vue
+++ src/components/task-preview.vue
@@ -28,12 +28,13 @@
         <component
           :is="cmpType"
           @closeModal="closeModal"
-          @toggleLabel="toggleLabel"
-          @toggleMember="toggleMember"
-          @setTaskStyle="setTaskStyle"
-          @addAttachment="addAttachment"
+          @editTask="editTask"
         >
         </component>
+
+      
+        <div @click.stop="removeTask"><span class="archive-icon"></span>Archive</div>
+
       </div>
       <div class="save-button">
         <button @click.stop="editTitle">Save</button>
@@ -83,6 +84,95 @@ export default {
     this.boardToEdit.groups.forEach((g) => (this.tasklen += g.tasks.length || 0))
   },
   methods: {
+    saveTask(userAction = 'Task update') {
+      this.$store.dispatch({
+        type: 'saveTask',
+        task: this.taskToEdit,
+        groupId: this.groupId,
+        boardId: this.boardId,
+        currBoard: this.getCurrBoard,
+        userAction,
+      })
+    },
+    editTask(editData) {
+      console.log(editData)
+      let idx
+      let userAction
+      switch (editData.type) {
+        case 'toggleMember':
+          const member = editData.data
+          console.log(member)
+          const members = this.taskToEdit.members
+          idx = members.findIndex((m) => m._id === member._id)
+          if (idx === -1) {
+            members.push(member)
+            userAction = 'Added member'
+          } else {
+            members.splice(idx, 1)
+            userAction = 'Removed member'
+          }
+          this.saveTask(userAction)
+          const notification = {
+            mentionedUserId: member._id,
+            userAction,
+            taskTitle: this.taskToEdit.title,
+            time: Date.now(),
+            style: this.taskToEdit.style,
+          }
+          socketService.emit(SOCKET_EMIT_MEMBER_ACTION, notification)
+          break
+
+        case 'toggleLabel':
+          const labelId = editData.data
+          const labels = this.taskToEdit.labelIds
+          idx = labels.findIndex((label) => label === labelId)
+          if (idx === -1) {
+            labels.push(labelId)
+            userAction = 'Added label'
+          } else {
+            labels.splice(idx, 1)
+            userAction = 'Removed label'
+          }
+          this.saveTask(userAction)
+          break
+
+        case 'addCheckList':
+          const checklist = editData.data
+          if (checklist.checkListTitle === '') return
+          this.taskToEdit.checklists.push(checklist)
+          this.saveTask('Added checklist')
+          this.isCheckListAdded = false
+          this.checkListTitle = ''
+          break
+
+        case 'setDate':
+          const dateValue = editData.data
+          this.taskToEdit.dueDate = dateValue
+          this.saveTask('Added due date')
+          break
+
+        case 'addAttachment':
+          const attachment = editData.data
+          this.taskToEdit.attachments.push(attachment)
+          this.saveTask('Added attchment')
+          break
+
+        case 'setTaskStyle':
+          const style = editData.data
+          this.taskToEdit.style = style
+          this.saveTask('Changed cover')
+          break
+      }
+    },
+    removeTask() {
+      this.$store.dispatch({
+        type: 'removeTask',
+        taskId: this.getCurrTask.id,
+        groupId: this.groupId,
+        boardId: this.boardId,
+      })
+      this.$router.push(`/board/${this.boardId}`)
+    },
     openTaskDetails() {
       this.$router.push(`/board/${this.boardToEdit._id}/${this.groupId}/${this.task.id}`)
       this.quickEditDisplay = "none"
@@ -106,40 +196,40 @@ export default {
     closeModal() {
       this.cmpType = null
     },
-    toggleLabel(labelId) {
-      const labels = this.taskToEdit.labelIds
-      const idx = labels.findIndex((label) => label === labelId)
-      let userAction = ""
-      if (idx === -1) {
-        userAction = "Added label"
-        labels.push(labelId)
-      } else {
-        labels.splice(idx, 1)
-        userAction = "Removed label"
-      }
-      this.saveTask(userAction)
-    },
-    toggleMember(member) {
-      const members = this.taskToEdit.members
-      const idx = members.findIndex((m) => m._id === member._id)
-       let userAction = ""
-      if (idx === -1) {
-        userAction = "Add member"
-        members.push(member)
-      } else {
-        members.splice(idx, 1)
-        userAction = "Removed member"
-      }
-      this.saveTask(userAction)
-    },
-    setTaskStyle(style) {
-      this.taskToEdit.style = style
-      this.saveTask("Changed cover")
-    },
-    addAttachment(attachment) {
-      this.taskToEdit.attachments.push(attachment)
-      this.saveTask("Added attchement")
-    },
+    // toggleLabel(labelId) {
+    //   const labels = this.taskToEdit.labelIds
+    //   const idx = labels.findIndex((label) => label === labelId)
+    //   let userAction = ""
+    //   if (idx === -1) {
+    //     userAction = "Added label"
+    //     labels.push(labelId)
+    //   } else {
+    //     labels.splice(idx, 1)
+    //     userAction = "Removed label"
+    //   }
+    //   this.saveTask(userAction)
+    // },
+    // toggleMember(member) {
+    //   const members = this.taskToEdit.members
+    //   const idx = members.findIndex((m) => m._id === member._id)
+    //    let userAction = ""
+    //   if (idx === -1) {
+    //     userAction = "Add member"
+    //     members.push(member)
+    //   } else {
+    //     members.splice(idx, 1)
+    //     userAction = "Removed member"
+    //   }
+    //   this.saveTask(userAction)
+    // },
+    // setTaskStyle(style) {
+    //   this.taskToEdit.style = style
+    //   this.saveTask("Changed cover")
+    // },
+    // addAttachment(attachment) {
+    //   this.taskToEdit.attachments.push(attachment)
+    //   this.saveTask("Added attchement")
+    // },
     addMemeberOnDrop(memberId) {
       let member = this.boardToEdit.members.find((member) => member._id === memberId)
       this.toggleMember(member)
